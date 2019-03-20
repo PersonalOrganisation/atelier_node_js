21e651454599cc798e5fb6c64d7ab7055d5d1cd0
Djamel <djamel.ammour@gmail.com>
2019-03-20 11:41:36 +0100

    wilders chalk

---
+++ main.js
@@ -0,0 +1,8 @@
+
+const chalk = require('chalk');
+
+    console.log(chalk.blue('Alex'));
+    console.log(chalk.blue('Nicolas'));
+    console.log(chalk.blue('Damien'));
+    console.log(chalk.blue('Bastien'));
+    console.log(chalk.blue('Sarah'));
